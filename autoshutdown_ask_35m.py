e5d9d6d468a5771b7570c38dd59ccb5c3a2b8f9a
WhiteCloud <78524792+WhiteCloudOL@users.noreply.github.com>
2023-09-17 07:37:01 +0800

    Update autoshutdown_ask_35m.py

--- autoshutdown_ask_35m.py
+++ autoshutdown_ask_35m.py
@@ -2,21 +2,19 @@
 AppName:Auto-Shutdown
 Description:Automatically shut down when the computer is idle.
 Created By WhiteCloudCN
-Version:V1.0pre
 """
 
 import os, ctypes, time, tkinter
 import threading
 import tkinter.messagebox
 
-sidletime = 1000 * 60 * 35  # 单位：ms
+sidletime = 1000 * 60 * 90  # 单位：ms
 waittime = 60 * 5  # 单位：s
 
 
 #对话框
 def qst():
-    if not tkinter.messagebox.askyesno('提示', '已打开自动关机程序，设定时间：%s 分钟\n是否运行，可在任务管理器关闭'%(int(sidletime/1000/60))):
-        os._exit(0)
+    tkinter.messagebox.showinfo('提示','已打开自动关机程序，设定时间：%s 分钟\n是否运行，可在任务管理器关闭'%(int(sidletime/1000/60)))
 
 
 def get_idle_time():
@@ -68,10 +66,9 @@ def app():
 
 #运行程序段
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        app_thread = threading.Thread(target=app)
-        qst_thread = threading.Thread(target=qst)
-        #启动询问进程
-        app_thread.start()
-        #启动主进程
-        qst_thread.start()
+    app_thread = threading.Thread(target=app)
+    qst_thread = threading.Thread(target=qst)
+    #启动询问进程
+    qst_thread.start()
+    #启动主进程
+    app_thread.start()
